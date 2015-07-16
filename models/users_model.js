35c391b694f240a0d7585674ab424c4dbdebb017
Syed A Hasan <[absaarcv@outlook.com]>
2015-07-16 09:06:59 +0100

    user model

--- models/users_model.js
+++ models/users_model.js
@@ -1,9 +1,26 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
+
+// Subdocument schema for user polls
+var UserPollsSchema = new Schema({
+    pollid: { type: String }
+});
+
+// Subdocument schema for user votes
+var UserVotesSchema = new Schema({
+    pollid: { type: String  },
+    vote: { type: String }
+
+});
+
+// User schema
+
 var UserSchema = new Schema({
     email: { type: String, unique: true, required: 'Email required', validate: [validateEmail, 'Please provide a valid email']  },
     username: { type: String, required: 'Username required'},
-    hashed_password: { type: String, required: 'Password required'  }
+    hashed_password: { type: String, required: 'Password required' },
+    polls: [UserPollsSchema],
+    votes: [UserVotesSchema]
 });
 
 function validateEmail(email) {
